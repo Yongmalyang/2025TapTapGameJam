cd45fc06693bcc444acecead8e4f9628125e3eb2
이주연 <37902200+Yongmalyang@users.noreply.github.com>
2025-06-20 23:27:57 +0900

    Feat: Collect Arm Weight (Only Left, with error)

---
+++ TapTapGameJam/Assets/Scripts/Player/ArmWeight.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class ArmWeight : MonoBehaviour
+{
+    public float AttatchSelfToArm(GameObject ConnectedBody, float curArmLength, bool isFirstArm, bool isLeft)
+    {
+        Rigidbody2D ConnectedRB = ConnectedBody.GetComponent<Rigidbody2D>();
+
+        // 조인트 부모 설정하기
+        HingeJoint2D hinge = GetComponent<HingeJoint2D>();
+        DistanceJoint2D distance = GetComponent<DistanceJoint2D>();
+
+        hinge.connectedBody = ConnectedRB;
+        distance.connectedBody = ConnectedRB;
+
+        // 붙일 접합면 정하기 (anchor)
+        float MyRatio = GetComponent<SpriteRenderer>().bounds.size.x;
+
+        Debug.Log("my ratio: "+MyRatio+", curArm: "+curArmLength);
+
+        // 왼쪽에 붙일지 오른쪽에 붙일지
+        float finalAnchor = 0;
+        if (isFirstArm)
+        {
+            if (isLeft) finalAnchor = -(ConnectedBody.GetComponent<SpriteRenderer>().bounds.size.x + MyRatio) / 2.0f;
+            else finalAnchor = (ConnectedBody.GetComponent<SpriteRenderer>().bounds.size.x + MyRatio) / 2.0f;
+        }
+        else
+        {
+            if (isLeft) finalAnchor = curArmLength - MyRatio;
+            else finalAnchor = curArmLength + MyRatio;
+        }
+
+        hinge.connectedAnchor = new Vector2(finalAnchor, hinge.connectedAnchor.y);
+        distance.connectedAnchor = new Vector2(finalAnchor, distance.connectedAnchor.y);
+
+        // 붙일 위치 정하기 (position)
+        Vector3 posA = ConnectedBody.transform.position;
+        float rotZ = ConnectedBody.transform.eulerAngles.z;
+        float dist = ConnectedBody.GetComponent<SpriteRenderer>().bounds.size.x + GetComponent<SpriteRenderer>().bounds.size.x;
+        
+        Vector3 offset = new Vector3(0, 0, 0);
+
+        if (isLeft) offset = Quaternion.Euler(0, 0, rotZ) * Vector3.left * dist;
+        else offset = Quaternion.Euler(0, 0, rotZ) * Vector3.right * dist;
+        
+        Vector3 posB = posA + offset;
+
+        transform.position = posB;
+
+        return finalAnchor;
+    }
+
+}
---
+++ TapTapGameJam/Assets/Scripts/Player/Player.cs
@@ -0,0 +1,79 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class Player : MonoBehaviour
+{
+    public float floatForce = 2f;        // 떠오르는 힘
+    public Transform capsuleSprite; // ← CapsuleSprite를 Inspector에 할당할 것
+    public float curArmLength = 0;
+
+    private Rigidbody2D rb;
+
+    [SerializeField]
+    private GameObject ArmWeightPrefab;
+    [SerializeField]
+    private GameObject LeftArm;
+    [SerializeField]
+    private GameObject RightArm;
+    [SerializeField]
+    private GameObject LeftLeg;
+    [SerializeField]
+    private GameObject RightLeg;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.A)) MovePlayer(-1, 1, 5f);
+        if (Input.GetKey(KeyCode.D)) MovePlayer(1, 1, -5f);
+        if (Input.GetKey(KeyCode.W)) MovePlayer(0, 1, 0);
+        if (Input.GetKey(KeyCode.S)) MovePlayer(0, -1, 0);
+    }
+
+    void MovePlayer(float dirX, float dirY, float rotateZ)
+    {
+        rb.velocity = new Vector2(dirX * floatForce, dirY * floatForce);
+        Quaternion targetRot = Quaternion.Euler(0, 0, rotateZ);
+        transform.localRotation = Quaternion.Lerp(transform.localRotation, targetRot, Time.deltaTime * 5f);
+
+    }
+
+    void AttachArmWeight(int index)
+    {
+        GameObject mainArm = LeftArm; //임시로
+        GameObject armWeight = Instantiate(ArmWeightPrefab, mainArm.transform);
+        float armWeightWidth = armWeight.GetComponent<SpriteRenderer>().bounds.size.x;
+
+        int childCount = mainArm.transform.childCount;
+
+        // 이미 생성되어서 붙어있는 시점에서의 팔 개수
+        // 이미 부착했으니까 자식 1개
+        if (childCount == 1)
+        {
+            // 자기 자신에게 붙이기
+            curArmLength = armWeight.GetComponent<ArmWeight>().AttatchSelfToArm(gameObject, curArmLength, true ,true);
+        }
+        else if(childCount > 1)
+        {
+            // 마지막 팔에 붙이기, 내 직전 친구
+            GameObject nthChild = mainArm.transform.GetChild(childCount-2).gameObject;
+            curArmLength = armWeight.GetComponent<ArmWeight>().AttatchSelfToArm(nthChild, curArmLength, false, true);
+        }
+
+        Debug.Log(curArmLength);
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("ArmWeight"))
+        {
+            AttachArmWeight(0);
+            Destroy(collision.gameObject);
+        }
+    }
+}
