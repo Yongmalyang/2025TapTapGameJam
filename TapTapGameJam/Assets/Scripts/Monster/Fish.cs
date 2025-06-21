e2dce946a8ab8a67b150e5ff06d4faf41028c2f2
이주연 <37902200+Yongmalyang@users.noreply.github.com>
2025-06-21 23:48:03 +0900

    Add: Fish Group

--- TapTapGameJam/Assets/Scripts/Monster/Fish.cs
+++ TapTapGameJam/Assets/Scripts/Monster/Fish.cs
@@ -2,15 +2,9 @@ using UnityEngine;
 
 public class Fish : BaseMonster
 {
-    public float moveSpeed = 2f;
-    public float moveRange = 3f;
-
     [Header("Attack Settings")]
     public float fishDamage = 10f;
     public float attackCooldown = 2f;
-
-    private Vector3 startPos;
-    private bool movingRight = true;
 
     private float lastAttackTime = -Mathf.Infinity;
     public bool canDealDamage = true;
@@ -19,22 +13,10 @@ public class Fish : BaseMonster
     {
         base.Start();
         oxygenDamage = fishDamage;
-        startPos = transform.position;
     }
 
     private void Update()
     {
-        float dir = movingRight ? 1 : -1;
-        transform.Translate(Vector2.right * moveSpeed * Time.deltaTime * dir);
-
-        if (Mathf.Abs(transform.position.x - startPos.x) >= moveRange)
-        {
-            movingRight = !movingRight;
-            Vector3 scale = transform.localScale;
-            scale.x *= -1;
-            transform.localScale = scale;
-        }
-
         // 공격 쿨타임 체크
         if (Time.time >= lastAttackTime + attackCooldown)
         {
