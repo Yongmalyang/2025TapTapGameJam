cf58bf6e16d66ca6b5ada333522af3f68e89d02d
이주연 <37902200+Yongmalyang@users.noreply.github.com>
2025-06-22 03:01:56 +0900

    Add: UI 적용

--- TapTapGameJam/Assets/Scripts/UI/MainUI.cs
+++ TapTapGameJam/Assets/Scripts/UI/MainUI.cs
@@ -1,11 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using TMPro;
 
 public class MainUI : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI weightText;
+    [SerializeField] private List<Sprite> stageUI;
+    [SerializeField] private Image mainBG;
     private string backPart;
 
     void Start()
@@ -15,8 +18,12 @@ public class MainUI : MonoBehaviour
 
     public void Init()
     {
-        backPart = " / " + GameManager.Instance.goalWeight[GameManager.Instance.curStageNum].ToString();
+        int curStage = GameManager.Instance.curStageNum;
+
+        backPart = " / " + GameManager.Instance.goalWeight[curStage].ToString() + "KG";
         weightText.text = "0" + backPart;
+
+        mainBG.sprite = stageUI[curStage];
     }
 
     public void UpdateUIText(int num)
