e43d5a361b7ae2eb13b6e6853e323037d37408b3
이주연 <37902200+Yongmalyang@users.noreply.github.com>
2025-06-22 15:50:46 +0900

    Feat: UI

--- TapTapGameJam/Assets/Scripts/IntroTutoManager.cs
+++ TapTapGameJam/Assets/Scripts/IntroTutoManager.cs
@@ -1,18 +1,46 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class IntroTutoManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public List<Sprite> sprites;      // 이미지 목록
+    public Image displayImage;        // 가운데 이미지
+    public Button leftButton;         // 왼쪽 버튼
+    public Button rightButton;        // 오른쪽 버튼
+
+    private int currentIndex = 0;
+
     void Start()
     {
-        
+        UpdateImage(); // 초기 이미지 표시
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnClickLeft()
     {
-        
+        if (currentIndex > 0)
+        {
+            currentIndex--;
+            UpdateImage();
+        }
+    }
+
+    public void OnClickRight()
+    {
+        if (currentIndex < sprites.Count - 1)
+        {
+            currentIndex++;
+            UpdateImage();
+        }
+    }
+
+    private void UpdateImage()
+    {
+        // 현재 이미지 갱신
+        displayImage.sprite = sprites[currentIndex];
+
+        // 양 끝에서 버튼 비활성화
+        leftButton.interactable = currentIndex > 0;
+        rightButton.interactable = currentIndex < sprites.Count - 1;
     }
 }
